5123d2360b93db8e1013ea6efc89f10554ce560e
Нестеров <myrz47@mail.ru>
2025-07-31 14:58:25 +0300

    update main.ts before deploy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3001',
+      'https://твой-домен.vercel.app'
+    ],
+    methods: ['GET', 'POST', 'DELETE'],
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
